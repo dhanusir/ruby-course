683d2c505f5e6fd0d79cd500705e3a5afc787659
Snehal Mohite <snehal1524@gmail.com>
2014-07-25 11:30:55 +0530

    Dai1 assignment completed

--- Day1/assignment.rb
+++ Day1/assignment.rb
@@ -2,31 +2,42 @@
 # --------------------------------------------------------------------------
 
 # 1. Write a method to swap two variables. 
-# def method(a, b)
-#   Your code here....
-# end
+def method(i, j)
+       i,j = j,i
+       puts i
+       puts "\n"
+       puts j
+end
 
-
+method(2,3)
 
 # 2. Write any one use case of === operator.
-# Your answer here...
+if (1...10) === 5
+puts "yay! it's available"
+else
+puts "Try another number"
+end
 
 
 
 # 3. Print array of alphabates using Range operator.
-# Your answer here...
+aray = ('a'..'z').to_a
+puts aray
+
 
 
 
 # 4. Print 'Ho! Ho! Ho! Merry Christmas!' using string interpolation and * operator.
-# Your answer here...
+message = "Mary Christmas!"
+puts "Ho!" * 3 + " #{message}"
 
 
 
 # 5. Write a ruby program that perform following operations:
-#    a. Ask user his/her name
-#    b. Ask user his/her age
-#    c. Finally, print result in the form
-#       "Your name is <user's name>"
-#       "Your age is <user's age>"
-# Your answer here...
+puts "Enter your name"
+name = gets
+puts "Enter your age"
+age = gets
+
+puts "Your name is #{name}"
+puts "Your age is #{age}"
