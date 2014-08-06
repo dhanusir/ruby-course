ccf3f6db04fe373e7a49470cf508d8f30b64c841
snehal89 <snehal.shinde@vertisinfotech.com>
2014-08-06 19:40:18 +0530

    String assignment completed

--- Day2/assignments/string_qs.rb
+++ Day2/assignments/string_qs.rb
@@ -1,19 +1,54 @@
 # Question1
 # Get the first letter from the string "Hello, Ruby!!".
 
+Answer-->
+
+puts "Hello, Ruby!!".slice(0)
+
+-------------------------------------------------------------
+
 # Question2
 # Get the first through 5th elements from the "Hello, Ruby!!" string.
+
+Answer-->
+puts "Hello, Ruby!!".slice(4)    # It will return 5th element of the string
+
+puts "Hello, Ruby!!".slice(0,5)       # It will return first to 5th elements of the string
+-------------------------------------------------------------
 
 # Question3
 # Check the 'sprintf' documentation and the % documentation in the String class
 # and write one line code to print exact following line
 # "1$ = Rs60.48"
 
+Answer-->
+
+-------------------------------------------------------------
+
 # Question4
 # What is the problem with the following code? Correct it and puts.
 # my variable = 'Mr. White'
 
+Answer-->  my_variable = 'Mr. White'   # Ruby does not understand space between the variable name.
+
+
+-------------------------------------------------------------
+
 # Question5
 # Print the result - Concatenate the following strings
-first = 'Beautiful '
+first = 'Beautiful'
 second = 'face tattoo'
+
+Answer-->
+
+1. puts first + second
+
+2. puts first.concat(second)
+
+3. puts first<<second   # It will concatinate two strings and saves the result into first string
+
+4. puts "#{first}#{second}"
+
+-------------------------------------------------------------
+
+
