bc74d09ca2825d94569ddfd935cdc77cb0e9eef5
vaishnavi <vaishnavigunwante@gmail.com>
2014-08-11 19:15:57 +0530

    Completed day 2 assignments

--- Day2/assignments/string_qs.rb
+++ Day2/assignments/string_qs.rb
@@ -1,19 +1,27 @@
 # Question1
 # Get the first letter from the string "Hello, Ruby!!".
+str = 'Hello, Ruby!!'
+puts str[0]
 
 # Question2
 # Get the first through 5th elements from the "Hello, Ruby!!" string.
+str = 'Hello, Ruby!!'
+puts str[0..5]
 
 # Question3
 # Check the 'sprintf' documentation and the % documentation in the String class
 # and write one line code to print exact following line
-# "1$ = Rs60.48"
+# 1$ = Rs60.48
+printf("%d$ = %g\n", 1, 60.48)
 
 # Question4
 # What is the problem with the following code? Correct it and puts.
 # my variable = 'Mr. White'
+my_variable = 'Mr. White'
+puts my_variable
 
 # Question5
 # Print the result - Concatenate the following strings
 first = 'Beautiful '
 second = 'face tattoo'
+puts first.concat(second)
