676ebad66c4aa64d43d5bcac4964b89b5b7b41ad
dineshb-qa <dineshbiradar95@gmail.com>
2014-08-14 20:27:03 +0530

    Completed with Day2 assignment

--- Day2/assignments/arrays_qs.rb
+++ Day2/assignments/arrays_qs.rb
@@ -1,28 +1,45 @@
+# Encoding: utf-8
 # Refer - http://www.ruby-doc.org/core-2.1.2/Array.html
 
 # Question1
 # Declare one array arr1 and method with array as parameter
 # return sum of elements in arr1 using each loop
+puts "\nAnswer for Question1 :"
 def sum_of_arr(arr)
-  # do whatever you want...
+  sum = 0
+  # Iterate over the array and make sum of the array elements
+  arr.each { |a| sum += a }
+  sum
 end
+
+arr1 = [1, 2, 3, 4, 5]
+puts "The sum of elements in arr1 is : #{sum_of_arr(arr1)}"
 
 # Question2
 # Following method 'push_elements_into_array' accepts two parameters
 # 1. array & 2. new_element which user want to insert into array
 # Method will return array with new element.
 # Add new_element into array using 'push' method of Array
+puts "\nAnswer for Question2 :"
 def push_elements_into_array(arr, ele)
-  # you can do it.. :-)
+  arr.push(ele)
 end
+
+arr2 = [1]
+puts "Array after adding 2 : #{push_elements_into_array(arr2, 2)}"
+puts "Array after adding 'key' : #{push_elements_into_array(arr2, 'key')}"
 
 # Question3
 # Pass array to below method
 # Iterate on array and puts each element from array
 # using 'pop' method of Array
+puts "\nAnswer for Question3 :"
 def pop_from_array(arr)
-  # you can do it.. :-)
+  (1..arr.length).each { puts "Popped Element from Array :#{arr.pop}" }
 end
+
+arr3 = [1, 2, 3, 4, 5]
+pop_from_array(arr3)
 
 # Question4
 # a. Declare one array b. sort the array using 'sort' method
@@ -31,7 +48,17 @@ end
 # eg. a = [1, 2, 3]
 # a.each_with_index { |n, index| .... }
 # Here, n = element of array & index = index of element in array
+puts "\nAnswer for Question4 :"
+arr4 = [2, 4, 1, 5, 3]
+puts "The Original Array is : #{arr4}"
+puts "Array After Sorting in Ascending Order is : #{arr4.sort}"
+puts "The Array in Reverse Order is : #{arr4.reverse}"
+puts "The Array Element with index=4 is : #{arr4[4]}"
 
 # Question5
 # str = 'Hello Ruby!!!'
 # Write code to store str into array as ['Hello fun', 'Ruby!!! fun']
+puts "\nAnswer for Question5 :"
+str = 'Hello Ruby!!!'
+arr5 = str.split(' ').each { |s| s.concat(' fun') }
+puts "The Array is : #{arr5}"
--- Day2/assignments/hash_qs.rb
+++ Day2/assignments/hash_qs.rb
@@ -1,3 +1,4 @@
+# Encoding: utf-8
 # Refer - http://www.ruby-doc.org/core-2.1.2/Hash.html
 
 # Question1
@@ -7,6 +8,8 @@ languages = {
   'java' => "don't know"
 }
 # Return the value that corresponds with the "ruby" key.
+puts "\nAnswer for Question1 :"
+puts "The value corresponding to key 'ruby' is #{languages['ruby']}"
 
 # Question2
 # Print value of browsers[:favorite]
@@ -15,6 +18,8 @@ browsers = {
   favorite: :firefox,
   worst: :internet_explorer
 }
+puts "\nAnswer for Question2 :"
+puts "Favorite Browser is : #{browsers[:favorite]}"
 
 # Question3
 # Use the cities hash to print out the following strings:
@@ -23,10 +28,22 @@ browsers = {
 
 cities = { santa_cruz: 'chill', new_york: 'intense' }
 
+puts "\nAnswer for Question3 :"
+cities.each { |key, value| puts "#{key} is #{value}" }
+
 # Question4
 # Only for practice:
 # See difference between the following lines of code
 
-box = { height: 10.5 }
-puts box.fetch(:size)
-puts box[:size]
+begin
+  box = { height: 10.5 }
+  puts box.fetch(:size)
+  puts box[:size]
+rescue
+  puts "\nAnswer for Question4 :"
+  puts "The line of code : 'puts box.fetch(:size)' throws an error.\
+  But the there is no error for the line : 'puts box[:size]'\
+  Because 'fetch' method does not handels the case of 'key does not exits',\
+  but 'box[:key]' method' handels this error\
+  and returns 'nil'. Hence there is no error for the 2nd puts."
+end
