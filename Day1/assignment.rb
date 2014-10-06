b29059b50b3ebdc1f3d43dac4bd1ddaa25b863e0
Anup.Kulkarni <anup.kulkarni@vertisinfotech.com>
2014-10-06 13:16:59 +0530

    assignment submitted

--- Day1/assignment.rb
+++ Day1/assignment.rb
@@ -2,24 +2,31 @@
 # --------------------------------------------------------------------------
 
 # 1. Write a method to swap two variables. 
-# def method(a, b)
-#   Your code here....
-# end
+def swap_num(a,b)
+   c=a
+   a=b
+   b=c
+   puts "A=#{a} B=#{b}"
+end
+swap_num(10,20)
+=>A=20 B=10
+  
+
 
 
 
 # 2. Write any one use case of === operator.
-# Your answer here...
+#
 
 
 
 # 3. Print array of alphabates using Range operator.
-# Your answer here...
-
+('a'..'h').to_a
+=> ["a","b","c","d","e","f","g","h"]
 
 
 # 4. Print 'Ho! Ho! Ho! Merry Christmas!' using string interpolation and * operator.
-# Your answer here...
+
 
 
 
@@ -29,4 +36,4 @@
 #    c. Finally, print result in the form
 #       "Your name is <user's name>"
 #       "Your age is <user's age>"
-# Your answer here...
+
