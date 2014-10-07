9038f9e6c45795d3574eff77457c1a0fbeede551
pankaj.borase <pankaj.borase@vertisinfitech.com>
2014-10-07 15:46:35 +0530

    asignment sumitted

--- Day1/assignment.rb
+++ Day1/assignment.rb
@@ -2,26 +2,32 @@
 # --------------------------------------------------------------------------
 
 # 1. Write a method to swap two variables. 
-# def method(a, b)
-#   Your code here....
-# end
+   def swap(a,b)
+   a=a+b
+   b=a-b
+   a=a-b
+   puts "a=#{a} b=#{b}"
+   end
+ => swap(20,50)
+
+  [50,20]
 
 
-
-# 2. Write any one use case of === operator.
-# Your answer here...
-
+# 2. Write any one use case of === aperator
+   (1..9) === 5.16732 returns true
+   
 
 
 # 3. Print array of alphabates using Range operator.
-# Your answer here...
+   ('a'..'m').to_a returns all alphabets starting from A to M. 
 
+ ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
 
-
-# 4. Print 'Ho! Ho! Ho! Merry Christmas!' using string interpolation and * operator.
-# Your answer here...
-
-
+# 4. Print 'Ho! Ho! Ho! Merry Christmas!' using string interpolation and * operato
+   a = 'Ho!' * 3
+   b = 'Merry christmas!'
+   puts a+b
+   Ho!Ho!Ho!Merry Cristmas!
 
 # 5. Write a ruby program that perform following operations:
 #    a. Ask user his/her name
@@ -29,4 +35,10 @@
 #    c. Finally, print result in the form
 #       "Your name is <user's name>"
 #       "Your age is <user's age>"
-# Your answer here...
+     @name = gets
+     @age = gets
+
+     puts "Your name is #{@name}"
+     puts "your name is #{@age}"
+  Your name is Pankaj
+  Your age is 25
