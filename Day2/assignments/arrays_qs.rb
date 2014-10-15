3c17943da5da1b9239151bfe5af19df5c87b3dd1
pankaj.borase <pankaj.borase@vertisinfotech.com>
2014-10-15 18:46:19 +0530

    Please check assignment2 of array

--- Day2/assignments/arrays_qs.rb
+++ Day2/assignments/arrays_qs.rb
@@ -4,7 +4,11 @@
 # Declare one array arr1 and method with array as parameter
 # return sum of elements in arr1 using each loop
 def sum_of_arr(arr)
-  # do whatever you want...
+   sum = 0
+   arr.each do |i|
+       sum = sum + i
+   end
+   sum
 end
 
 # Question2
@@ -13,7 +17,8 @@ end
 # Method will return array with new element.
 # Add new_element into array using 'push' method of Array
 def push_elements_into_array(arr, ele)
-  # you can do it.. :-)
+   arr.push(ele)
+    arr
 end
 
 # Question3
@@ -21,7 +26,11 @@ end
 # Iterate on array and puts each element from array
 # using 'pop' method of Array
 def pop_from_array(arr)
-  # you can do it.. :-)
+   l=arr.length
+   l.each do
+      num=arr.pop
+      puts "#{num} is removed!"
+   end
 end
 
 # Question4
@@ -31,7 +40,16 @@ end
 # eg. a = [1, 2, 3]
 # a.each_with_index { |n, index| .... }
 # Here, n = element of array & index = index of element in array
-
+ a=[9,2,4,6,8,1,3,7,5,10]
+=> [9, 2, 4, 6, 8, 1, 3, 7, 5, 10]
+a.sort
+=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a.reverse
+=> [10, 5, 7, 3, 1, 8, 6, 4, 2, 9]
+puts a[4]
+=> 8
 # Question5
 # str = 'Hello Ruby!!!'
 # Write code to store str into array as ['Hello fun', 'Ruby!!! fun']
+arr = str.split(/ /)
+arr.map {|i| i +" fun"}
